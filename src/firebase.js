493de7eb1947c357c894145ee989435ef9e02dee
Casmir Onyeka <casmir293@gmail.com>
2023-11-12 08:20:48 +0100

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDa6K26zQCeqtSiqCcHTGKL98FClH9TUEs",
+  authDomain: "polished-talon-393921.firebaseapp.com",
+  projectId: "polished-talon-393921",
+  storageBucket: "polished-talon-393921.appspot.com",
+  messagingSenderId: "827368481395",
+  appId: "1:827368481395:web:52ea5d0baff133da8f0006",
+};
