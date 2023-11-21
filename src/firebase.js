19323e0cac7716e48e592e727f9269d786aa04bd
Casmir Onyeka <casmir293@gmail.com>
2023-11-21 11:46:45 +0100

    firebase file setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDa6K26zQCeqtSiqCcHTGKL98FClH9TUEs",
   authDomain: "polished-talon-393921.firebaseapp.com",
@@ -6,3 +9,17 @@ const firebaseConfig = {
   messagingSenderId: "827368481395",
   appId: "1:827368481395:web:52ea5d0baff133da8f0006",
 };
+
+// Initialize firebase app
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const db = getFirestore();
+
+// Collection ref
+const colRef = collection(db, "books");
+
+// Get collection data
+getDocs(colRef).then((snapshot) => {
+  console.log(snapshot.docs);
+});
